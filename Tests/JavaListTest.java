43c25c3f22df907c641fbc0d85f98ed7319cd092
Britt Ballard <britt@thoughtbot.com>
2014-09-30 14:25:01 -0500

    Initial commit

---
+++ Tests/JavaListTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class JavaListTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/JavaLint.java
@@ -0,0 +1,7 @@
+
+public class JavaLint {
+
+public static void main(String[] args) {
+
+  }
+}
